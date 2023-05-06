0e239afb9b56fe933df9b2ce24ea10b632aef93a
hassans2022skipq <hassan.siddiqui.skipq@gmail.com>
2023-05-06 07:49:58 +0500

    precautionary commit

--- Frontend/src/components/Register.jsx
+++ Frontend/src/components/Register.jsx
@@ -1,10 +1,63 @@
 import { useRef, useState } from "react";
 import { SimpleGrid, Flex, Input, Button, VisuallyHidden } from "@chakra-ui/react";
 import { AiFillCamera } from 'react-icons/ai'
+import axios from "axios";
 
 const Register = () => {
     const [file, setFile] = useState(null);
     const hiddenFileInput = useRef(null);
+    const [error, setError] = useState(null);
+    const [success, setSuccess] = useState(null);
+    const [userObject, setUserObject] = useState({
+        username: "",
+        email: "",
+        password: "",
+        profile: null
+    });
+
+
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(userObject);
+        if (userObject.username === "" || userObject.email === "" || userObject.password === "") {
+            setError("Please fill all the fields");
+            setTimeout(() => {
+                setError(null);
+            }, 3000);
+            return;
+        }
+        if (userObject.username.length < 6) {
+            setError("Username must be atleast 6 characters long");
+            setTimeout(() => {
+                setError(null);
+            }, 3000);
+            return;
+        }
+        if (!userObject.email.includes("@")) {
+            setError("Email must be valid");
+            setTimeout(() => {
+                setError(null);
+            }, 3000);
+            return;
+        }
+        if (userObject.password.length < 6) {
+            setError("Password must be atleast 6 characters long");
+            setTimeout(() => {
+                setError(null);
+            }, 3000);
+            return;
+        }
+        if (userObject.profile === null) {
+            setError("Please upload a profile picture");
+            setTimeout(() => {
+                setError(null);
+            }, 3000);
+            return;
+        }
+
+        setSuccess("User Registered Successfully");
+    };
+
 
     const handleClick = event => {
         hiddenFileInput.current.click();
@@ -16,7 +69,7 @@ const Register = () => {
     };
 
     return (
-        <>
+        <form>
             <SimpleGrid
                 columns={1}
                 px={6}
@@ -27,17 +80,33 @@ const Register = () => {
                     color: "gray.700",
                 }}
             >
+                {error && <p style={{ color: "red" }}>{error}</p>}
                 <Flex>
                     <VisuallyHidden>User Name</VisuallyHidden>
-                    <Input mt={0} type="text" placeholder="User Name" />
+                    <Input mt={0} type="text" placeholder="User Name" onChange={(e) => {
+                        setUserObject({
+                            ...userObject,
+                            username: e.target.value
+                        })
+                    }} />
                 </Flex>
                 <Flex>
                     <VisuallyHidden>Email Address</VisuallyHidden>
-                    <Input mt={0} type="email" placeholder="Email Address" />
+                    <Input mt={0} type="email" placeholder="Email Address" onChange={(e) => {
+                        setUserObject({
+                            ...userObject,
+                            email: e.target.value
+                        })
+                    }} />
                 </Flex>
                 <Flex>
                     <VisuallyHidden>Password</VisuallyHidden>
-                    <Input mt={0} type="password" placeholder="Password" />
+                    <Input mt={0} type="password" placeholder="Password" onChange={(e) => {
+                        setUserObject({
+                            ...userObject,
+                            password: e.target.value
+                        })
+                    }} />
                 </Flex>
                 {!file ? <Flex>
                     <VisuallyHidden>Profile Picture</VisuallyHidden>
@@ -68,12 +137,13 @@ const Register = () => {
                     background: "#Ed2727",
                     color: "#ffffff",
                     border: "1px solid #ED2727"
-                }} w="full" mt={4} py={2} type="submit">
+                }} w="full" mt={4} py={2} onClick={handleSubmit}>
                     Sign up for free
                 </Button>
             </SimpleGrid >
-        </>
+        </form>
     )
+
 }
 
-export default Register
+export default Register;
